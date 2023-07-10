8e0a84c6f272ea20ff61f7e14016e816c51eab25
quocthinh79 <19130215@st.hcmuaf.edu.vn>
2023-07-10 21:27:21 +0700

    add token to meta data request

--- src/core/api/instance-axios/index.ts
+++ src/core/api/instance-axios/index.ts
@@ -1,13 +1,16 @@
-import { API_SPRING_BOOT } from '@constant';
-import axios from 'axios';
+import { API_SPRING_BOOT } from "@constant";
+import axios from "axios";
 
 const instanceAxios = axios.create({
-    baseURL: API_SPRING_BOOT,
-    headers: {
-        'Content-Type': 'application/json',
-        'Access-Control-Allow-Origin': '*',
-        'Access-Control-Allow-Headers': 'Content-Type, Authorization, X-Requested-With',
-    },
+  baseURL: API_SPRING_BOOT,
+  headers: {
+    Authorization:
+      "Bearer " + JSON.parse(localStorage.getItem("token") || "").state.token,
+    "Content-Type": "application/json",
+    "Access-Control-Allow-Origin": "*",
+    "Access-Control-Allow-Headers":
+      "Content-Type, Authorization, X-Requested-With",
+  },
 });
 
 export default instanceAxios;
