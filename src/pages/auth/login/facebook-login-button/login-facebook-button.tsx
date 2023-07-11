56979de61c0441775e13cf8bf7f40c064bce2826
quocthinh79 <19130215@st.hcmuaf.edu.vn>
2023-07-11 22:33:05 +0700

    add css

--- src/pages/auth/login/facebook-login-button/login-facebook-button.tsx
+++ src/pages/auth/login/facebook-login-button/login-facebook-button.tsx
@@ -1,17 +1,34 @@
-import FacebookLogin, { ReactFacebookFailureResponse, ReactFacebookLoginInfo } from 'react-facebook-login';
-import { FacebookOutlined } from '@ant-design/icons';
+import FacebookLogin, {
+  ReactFacebookFailureResponse,
+  ReactFacebookLoginInfo,
+} from "react-facebook-login";
+import { FacebookOutlined } from "@ant-design/icons";
+import styled from "@emotion/styled";
+import { templateStringToClassName } from "@core";
 
-const FacebookButton = ({ onFacebookResponse }: { onFacebookResponse: (response: ReactFacebookLoginInfo | ReactFacebookFailureResponse) => void }) => {
-  const responseFacebook = (response: ReactFacebookLoginInfo | ReactFacebookFailureResponse) => {
+const FacebookButton = ({
+  onFacebookResponse,
+}: {
+  onFacebookResponse: (
+    response: ReactFacebookLoginInfo | ReactFacebookFailureResponse
+  ) => void;
+}) => {
+  const responseFacebook = (
+    response: ReactFacebookLoginInfo | ReactFacebookFailureResponse
+  ) => {
     onFacebookResponse(response);
   };
 
   return (
     <FacebookLogin
+      cssClass={templateStringToClassName()`
+        border-radius: 10px;
+        background-color: #fff;
+      `}
       appId="1217081152339317"
       autoLoad={false}
       fields="name,email,picture"
-      textButton='Login with Facebook'
+      textButton="Login with Facebook"
       icon={<FacebookOutlined />}
       callback={responseFacebook}
     />
